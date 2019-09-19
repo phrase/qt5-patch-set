6457545005ae00b9635bd19f1c2f58d32f445cb6
Michal Lazo <michal.lazo@memsource.com>
2019-09-19 08:46:50 -0700

    fix qtwebkit build

--- apply.bat
+++ apply.bat
@@ -49,5 +49,6 @@ cd qtwebkit
 "C:\Program Files\Git\usr\bin\patch.exe" -p1 < 0001-build-without-qt-pdf-support.patch
 "C:\Program Files\Git\usr\bin\patch.exe" -p1 < 0010-disable-useless-features.patch
 "C:\Program Files\Git\usr\bin\patch.exe" -p1 < fixes-for-gcc-v7.3.0.patch
+"C:\Program Files\Git\usr\bin\patch.exe" -p1 < 0001-fix-build-without-webkit2.patch
 cd ..
 
--- apply.sh
+++ apply.sh
@@ -34,4 +34,5 @@ cd qtwebkit
 patch -p1 < 0001-build-without-qt-pdf-support.patch
 patch -p1 < 0010-disable-useless-features.patch
 patch -p1 < fixes-for-gcc-v7.3.0.patch
+patch -p1 < 0001-fix-build-without-webkit2.patch
 cd ..
