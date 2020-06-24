ad7c846b9212adf3b69718ea58760da3195b9443
Michal Lazo <michal.lazo@memsource.com>
2020-06-24 12:50:21 +0200

    revert include back

--- apply.bat
+++ apply.bat
@@ -38,4 +38,5 @@ cd ..
 cd qtwebengine
 "C:\Program Files\Git\usr\bin\patch.exe" -p1 < 0001-enable-ninjaflags-as-env-variable.patch
 "C:\Program Files\Git\usr\bin\patch.exe" -p1 < 0002-disable-useless-check.patch
+"C:\Program Files\Git\usr\bin\patch.exe" -p1 < 0003-missing-include.patch
 cd ..
